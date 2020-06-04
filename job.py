2b78eabf6c5c86a47b6a67ccb486d039280307b9
Artur Owczarek <oartur618@gmail.com>
2020-06-04 23:18:28 +0200

    Looking for jobs on nofluffjobs.com done

---
+++ job.py
@@ -0,0 +1,28 @@
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def job_search(location, technology):
+    """Method to look for a job in the Internet."""
+    page_number = 1
+    job_links = []
+    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36"}
+
+    while True:
+        nofluffjobs_url = f"https://nofluffjobs.com/pl/jobs/{location}/{technology}?criteria=city%3D{location}%20{technology}&page={page_number}"
+
+        page = requests.get(nofluffjobs_url, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        for a in soup.findAll('a', class_=re.compile("^posting-list-item posting-list-item--"), href=True):
+            if a['href'] not in job_links:
+                print('dupa')
+                job_links.append(a['href'])
+            else:
+                break
+
+        page_number += 1
+
+    page_number = 1
+    # TODO: search for a jon on justjoin.it site
---
+++ main.py
@@ -0,0 +1,10 @@
+import job
+
+
+def main():
+    """Main function"""
+    job.job_search("Wroclaw", "Python")
+
+
+if __name__ == '__main__':
+    main()
