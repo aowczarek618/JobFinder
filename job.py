c17acd3e27a222b52ab43f508e95139e05d3b94b
Artur Owczarek <oartur618@gmail.com>
2020-06-05 10:54:25 +0200

    .

--- job.py
+++ job.py
@@ -14,10 +14,11 @@ def job_search(location, technology):
         nofluffjobs_url = f"https://nofluffjobs.com/pl/jobs/{location}/{technology}?criteria=city%3D{location}%20{technology}&page={page_number}"
 
         page = requests.get(nofluffjobs_url, headers=headers)
+        json_data = page.json()
+        print(json_data)
         soup = BeautifulSoup(page.content, 'html.parser')
         for a in soup.findAll('a', class_=re.compile("^posting-list-item posting-list-item--"), href=True):
             if a['href'] not in job_links:
-                print('dupa')
                 job_links.append(a['href'])
             else:
                 break
@@ -25,4 +26,4 @@ def job_search(location, technology):
         page_number += 1
 
     page_number = 1
-    # TODO: search for a jon on justjoin.it site
+    # TODO: search for a job on justjoin.it site
