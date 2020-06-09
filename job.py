c064277d751af35a838c55d1ddf3bdc930267223
Artur Owczarek <oartur618@gmail.com>
2020-06-10 00:17:08 +0200

    Final version

--- job.py
+++ job.py
@@ -3,27 +3,86 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+HEADERS = {
+    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36"}
+
 
 def job_search(location, technology):
     """Method to look for a job in the Internet."""
     page_number = 1
-    job_links = []
-    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36"}
+    job_links = set()
+    job_links_quantity = len(job_links)
 
     while True:
         nofluffjobs_url = f"https://nofluffjobs.com/pl/jobs/{location}/{technology}?criteria=city%3D{location}%20{technology}&page={page_number}"
 
-        page = requests.get(nofluffjobs_url, headers=headers)
-        json_data = page.json()
-        print(json_data)
+        page = requests.get(nofluffjobs_url, headers=HEADERS)
         soup = BeautifulSoup(page.content, 'html.parser')
-        for a in soup.findAll('a', class_=re.compile("^posting-list-item posting-list-item--"), href=True):
-            if a['href'] not in job_links:
-                job_links.append(a['href'])
-            else:
-                break
-
+        for a in soup.findAll('a', class_=re.compile("^posting-list-item posting-list-item--"),
+                              href=True):
+            job_links.add(f"https://nofluffjobs.com{a['href']}")
+        if job_links_quantity == len(job_links):
+            break
+        job_links_quantity = len(job_links)
         page_number += 1
 
-    page_number = 1
-    # TODO: search for a job on justjoin.it site
+    justjoin_api_url = 'https://justjoin.it/api/offers'
+    page = requests.get(justjoin_api_url, headers=HEADERS)
+    justjoin_job_offers = page.json()
+    for job_offer in justjoin_job_offers:
+        if job_offer['city'] == location and job_offer['marker_icon'] == technology:
+            job_links.add(justjoin_make_job_link(job_offer["title"], job_offer['company_name'],
+                                                 job_offer['city']))
+
+    return job_links
+
+
+def remove_accents(input_text):
+    """
+    Function useful for web scraping.
+    Zażółć gęślą jaźń --> Zazolc gesla jazn
+    """
+    strange = 'ŮôῡΒძěἊἦëĐᾇόἶἧзвŅῑἼźἓŉἐÿἈΌἢὶЁϋυŕŽŎŃğûλВὦėἜŤŨîᾪĝžἙâᾣÚκὔჯᾏᾢĠфĞὝŲŊŁČῐЙῤŌὭŏყἀхῦЧĎὍОуνἱῺèᾒῘᾘὨШūლἚύсÁóĒἍŷöὄЗὤἥბĔõὅῥŋБщἝξĢюᾫაπჟῸდΓÕűřἅгἰშΨńģὌΥÒᾬÏἴქὀῖὣᾙῶŠὟὁἵÖἕΕῨčᾈķЭτἻůᾕἫжΩᾶŇᾁἣჩαἄἹΖеУŹἃἠᾞåᾄГΠКíōĪὮϊὂᾱიżŦИὙἮὖÛĮἳφᾖἋΎΰῩŚἷРῈĲἁéὃσňİΙῠΚĸὛΪᾝᾯψÄᾭêὠÀღЫĩĈμΆᾌἨÑἑïოĵÃŒŸζჭᾼőΣŻçųøΤΑËņĭῙŘАдὗპŰἤცᾓήἯΐÎეὊὼΘЖᾜὢĚἩħĂыῳὧďТΗἺĬὰὡὬὫÇЩᾧñῢĻᾅÆßшδòÂчῌᾃΉᾑΦÍīМƒÜἒĴἿťᾴĶÊΊȘῃΟúχΔὋŴćŔῴῆЦЮΝΛῪŢὯнῬũãáἽĕᾗნᾳἆᾥйᾡὒსᾎĆрĀüСὕÅýფᾺῲšŵкἎἇὑЛვёἂΏθĘэᾋΧĉᾐĤὐὴιăąäὺÈФĺῇἘſგŜæῼῄĊἏØÉПяწДĿᾮἭĜХῂᾦωთĦлðὩზკίᾂᾆἪпἸиᾠώᾀŪāоÙἉἾρаđἌΞļÔβĖÝᾔĨНŀęᾤÓцЕĽŞὈÞუтΈέıàᾍἛśìŶŬȚĳῧῊᾟάεŖᾨᾉςΡმᾊᾸįᾚὥηᾛġÐὓłγľмþᾹἲἔбċῗჰხοἬŗŐἡὲῷῚΫŭᾩὸùᾷĹēრЯĄὉὪῒᾲΜᾰÌœĥტ'
+
+    ascii_replacements = 'UoyBdeAieDaoiiZVNiIzeneyAOiiEyyrZONgulVoeETUiOgzEaoUkyjAoGFGYUNLCiIrOOoqaKyCDOOUniOeiIIOSulEySAoEAyooZoibEoornBSEkGYOapzOdGOuraGisPngOYOOIikoioIoSYoiOeEYcAkEtIuiIZOaNaicaaIZEUZaiIaaGPKioIOioaizTIYIyUIifiAYyYSiREIaeosnIIyKkYIIOpAOeoAgYiCmAAINeiojAOYzcAoSZcuoTAEniIRADypUitiiIiIeOoTZIoEIhAYoodTIIIaoOOCSonyKaAsSdoACIaIiFIiMfUeJItaKEISiOuxDOWcRoiTYNLYTONRuaaIeinaaoIoysACRAuSyAypAoswKAayLvEaOtEEAXciHyiiaaayEFliEsgSaOiCAOEPYtDKOIGKiootHLdOzkiaaIPIIooaUaOUAIrAdAKlObEYiINleoOTEKSOTuTEeiaAEsiYUTiyIIaeROAsRmAAiIoiIgDylglMtAieBcihkoIrOieoIYuOouaKerYAOOiaMaIoht'
+
+    translator = str.maketrans(strange, ascii_replacements)
+
+    return input_text.translate(translator)
+
+
+def justjoin_make_job_link(title, company, location):
+    """
+    Create job link from job title, company which announced job offer
+    and job location for justjoin.it website.
+    """
+
+    company_url = '-'.join(company.lower().split(' '))
+    title_url = re.sub(r'[\(\)]', '', '-'.join(title.lower().split(' ')))
+    title_url = re.sub(r'/', '-', title_url)
+    location_url = remove_accents(location.lower())
+
+    justjoin_url = f"https://justjoin.it/offers/{company_url}-{title_url}-{location_url}"
+    justjoin_api_url = f"https://justjoin.it/api/offers/{company_url}-{title_url}-{location_url}"
+
+    page = requests.get(justjoin_api_url, headers=HEADERS)
+    if page.status_code == 200:
+        return justjoin_url
+    return f"https://justjoin.it/offers/{company_url}-{title_url}"
+
+
+def write_job_links_to_file(filename, job_links):
+    """Write job links to the file."""
+    with open(filename, 'w') as file:
+        for job_link in job_links:
+            file.write(f"{job_link}\n")
+
+
+def read_job_links_from_file(filename):
+    """Read job links from the file."""
+    job_links = set()
+    with open(filename, 'r') as file:
+        for line in file:
+            job_links.add(line.rstrip())
+
+    return job_links
--- main.py
+++ main.py
@@ -1,10 +1,43 @@
+#!/usr/bin/python3
+
+import os
+import smtplib
+
 import job
 
 
 def main():
-    """Main function"""
-    job.job_search("Wroclaw", "Python")
+    """Main function: collect available job offers links and send email then."""
+
+    # Create a file if doesn't exists and doesn't overwrite if exists.
+    filename = 'job_links_old'
+    with open(filename, 'a') as file:
+        pass
+
+    job_links_old = job.read_job_links_from_file(filename)
+    job_links = job.job_search(LOCATION, TECHNOLOGY)
+    job_links_new = job_links - job_links_old
+    job.write_job_links_to_file(filename, job_links)
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        subject = 'New job offers are available!'
+        body = f'Hi me,\nI have found some new job {TECHNOLOGY.capitalize()} offers in {job.remove_accents(LOCATION)} for you:\n\n'
+        if len(job_links_new) != 0:
+            for job_link in job_links_new:
+                body += f'{job_link}\n'
+
+            msg = f'Subject: {subject}\n\n{body}'
+            smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
 
 
 if __name__ == '__main__':
+    EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+    EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    LOCATION = 'Wrocław'
+    TECHNOLOGY = 'python'
     main()
